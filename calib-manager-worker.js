cfd6b8fd882ffe591a79f5b474d11452eba680db
Jake Danczyk <jake@pocketnc.com>
2022-10-07 13:00:56 -0600

    CalibManagerWorker add 'connected' variable

--- calib-manager-worker.js
+++ calib-manager-worker.js
@@ -7,17 +7,20 @@ class CalibManagerWorker {
   constructor(
     calibProcess
   ) {
+    this.connected = false;
     this.calibProcess = calibProcess;
 
     this.calibSocket = zmq.socket("pull");
 
     this.calibSocket.on('connectFailed', (error) => {
+      this.connected = false;
       setTimeout(this.connect, 3000);
       console.log('Calib Connect Error: ' + error.toString());
     });
 
     this.calibSocket.on('connect', (connection) => {
       console.log('Calib Connection established!');
+      this.connected = true;
     });
 
     this.calibSocket.on("message", async (msg) => {
@@ -25,11 +28,18 @@ class CalibManagerWorker {
       var calibManagerReport = JSON.parse(msg);
       await this.calibProcess.receiveUpdate(calibManagerReport);
     });
+
+    this.calibSocket.on('close', (reasonCode, description) => {
+      this.connected = false;
+      console.log('Calib ZMQ connection closed: ' + description.toString());
+    });
   }
 
   connect() {
     console.log('attempting connect calib')
     this.calibSocket.connect('ipc:///tmp/cmm');
+    console.log('after connect calib')
+
   }
 }
 
