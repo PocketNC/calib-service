a446f00e3237d173f457bd5c4febdc467fb262c6
John Allwine <john@pocketnc.com>
2023-02-28 14:03:21 -0700

    added upload files script for uploading calibration data after cut test SOFT-1051

---
+++ startUploadFiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -f /opt/pocketnc/pocketnc_env ]; then
+  source /opt/pocketnc/pocketnc_env
+fi
+if [ -f /home/pocketnc/.pocketnc_env ]; then
+  source /home/pocketnc/.pocketnc_env
+fi
+
+cd /opt/calib-service
+
+# pass CalibService as argument on command line so we
+# can grep for it when looking for a process id to
+# kill with stopCalib.sh
+nohup node upload_files.js $1 $2 CalibService >> /var/opt/pocketnc/calib/calib-service.log 2>&1 &
---
+++ upload_files.js
@@ -0,0 +1,34 @@
+const util = require("util");
+const { exec } = require("child_process");
+const execPromise = util.promisify(exec);
+const fs = require('fs');
+
+const { CalibProcess } = require('./calib');
+const { CalibManagerWorker } = require('./calib-manager-worker');
+
+
+var serialNum = process.argv[2];
+var v2variant = process.argv[3];
+
+var calibProcess = new CalibProcess(serialNum, v2variant, "BASIC");
+var calibManagerWorker = new CalibManagerWorker(calibProcess);
+calibManagerWorker.connect();
+
+async function main() {
+  console.log(process.versions);
+  console.log(`Starting calibration upload. serial ${serialNum}, spindle RPM ${v2variant}k`);
+
+  await execPromise('connmanctl config $(connmanctl services | egrep -o "ethernet.*$") --ipv4 manual 10.0.0.101 255.255.255.0');
+
+  while(true){
+    await new Promise(r => setTimeout(r, 1000));
+    if(calibProcess.commandServer.isConnected() && calibProcess.rockhopperClient.connected){
+      break;
+    }
+  }
+
+  await calibProcess.runUploadFiles();
+  process.exit(0);
+}
+
+main()
